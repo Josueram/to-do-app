b0f6183bc3e62f031aaffdde586cfb2b2f790c01
Josue Ramirez <josueramirez0110@gmail.com>
2022-02-23 17:13:53 -0600

    The structure of the application components was defined

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
+import React from 'react';
+import { TodoCounter } from './components/TodoCounter';
+import { TodoSearch } from './components/TodoSearch';
+import { TodoList } from './components/TodoList';
+import { TodoItem } from './components/TodoItem';
+import { CreateTodoButton } from './components/CreateTodoButton';
 // import './App.css';
+
+const todos = [
+  { text: 'Hacer una landing page', completed: false },
+  { text: 'Tomar clases de desarrollo web', completed: false },
+  { text: 'Asistir a la reunión de Google', completed: false },
+]
 
 function App() {
   return (
-    
+    <React.Fragment>
+      <TodoCounter />
+      <TodoSearch />
+      
+      <TodoList>
+        {todos.map(todo => (
+          <TodoItem key={todo.text} text={todo.text} />
+        ))}
+      </TodoList>
+
+      <CreateTodoButton />
+      
+    </React.Fragment>
   );
 }
 
